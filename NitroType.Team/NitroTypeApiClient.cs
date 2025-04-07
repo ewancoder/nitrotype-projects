bbb42870882259d58572c752674118bca4caa6a2
Ivan Zyranau <ewancoder@gmail.com>
2025-04-07 15:24:21 +0400

    Add more cool info & Accuracy DELTA

--- NitroType.Team/NitroTypeApiClient.cs
+++ NitroType.Team/NitroTypeApiClient.cs
@@ -4,6 +4,8 @@ namespace NitroType.Team;
 
 public sealed class NitroTypeApiClient
 {
+    private ApiData _previousData;
+    private DateTime _previousDataQueriedAt;
     private ApiData _cache;
     private DateTime _lastQueriedAt;
 
@@ -20,6 +22,21 @@ public sealed class NitroTypeApiClient
             _lastQueriedAt = DateTime.UtcNow;
         }
 
+        if (DateTime.UtcNow - _previousDataQueriedAt > TimeSpan.FromDays(1))
+        {
+            _previousData = _cache;
+            _previousDataQueriedAt = DateTime.UtcNow;
+        }
+
+        foreach (var catInfo in _cache.results.season)
+        {
+            var previous = _previousData.results.season.FirstOrDefault(x => x.username == catInfo.username);
+            if (previous != null)
+            {
+                catInfo.AccuracyImprovement = catInfo.Accuracy - previous.Accuracy;
+            }
+        }
+
         return _cache.results.season;
     }
 }
@@ -34,4 +51,45 @@ public sealed record CatInfo(string username, string displayName, long typed, lo
     public decimal Accuracy => 100m * (typed - errs) / typed;
     public decimal AverageTextLength => (decimal)typed / played;
     public decimal AverageSpeed => (60m / 5) * typed / secs;
+    public decimal AccuracyImprovement { get; set; } = 0m;
+
+    public string TimeSpent
+    {
+        get
+        {
+            var time = TimeSpan.FromSeconds(secs);
+            var parts = new List<string>();
+            if (time.Days > 0)
+                parts.Add($"{time.Days} day{(time.Days > 1 ? "s" : "")}");
+            if (time.Hours > 0)
+                parts.Add($"{time.Hours} hour{(time.Hours > 1 ? "s" : "")}");
+            if (time.Minutes > 0)
+                parts.Add($"{time.Minutes} minute{(time.Minutes > 1 ? "s" : "")}");
+            if (time.Seconds > 0)
+                parts.Add($"{time.Seconds} second{(time.Minutes > 1 ? "s" : "")}");
+
+            return string.Join(" ", parts);
+        }
+    }
+
+    public string AccuracyImprovementString
+    {
+        get
+        {
+            if (AccuracyImprovement > 0) return $"\u25b2 {AccuracyImprovement.ToString("#.#####")}";
+            if (AccuracyImprovement < 0) return $"\u25bc {(-AccuracyImprovement).ToString("#.#####")}";
+
+            return string.Empty;
+        }
+    }
+
+    public string AccuracyClass
+    {
+        get
+        {
+            if (AccuracyImprovement > 0) return "good";
+            if (AccuracyImprovement < 0) return "bad";
+            return string.Empty;
+        }
+    }
 }
