19519a6c337b3b2a6d15f84d96a3363c0d4300e8
Ivan Zyranau <ewancoder@gmail.com>
2025-04-09 05:26:05 +0400

    Properly implement deltas

--- NitroType.Team/NitroTypeApiClient.cs
+++ NitroType.Team/NitroTypeApiClient.cs
@@ -4,45 +4,79 @@ namespace NitroType.Team;
 
 public sealed class NitroTypeApiClient
 {
-    private ApiData _previousData;
-    private DateTime _previousDataQueriedAt;
-    private ApiData _cache;
-    private DateTime _lastQueriedAt;
+    private List<ApiData> _data = [];
+    private static readonly TimeSpan _deltaInterval = TimeSpan.FromDays(1);
+
+    public NitroTypeApiClient()
+    {
+        _ = Task.Run(async () =>
+        {
+            while (true)
+            {
+                await Task.Delay(TimeSpan.FromMinutes(1));
+                try
+                {
+                    _ = await GetCatsInfoAsync();
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine("Error: " + exception);
+                }
+            }
+        });
+    }
 
     public async ValueTask<IEnumerable<CatInfo>> GetCatsInfoAsync()
     {
-        if (DateTime.UtcNow - _lastQueriedAt > TimeSpan.FromSeconds(20))
+        if (_data.Count == 0 || DateTime.UtcNow - _data.Last().Timestamp > TimeSpan.FromMinutes(1))
         {
             var client = new HttpClient();
             var data = await client.GetAsync("https://www.nitrotype.com/api/v2/teams/KECATS");
             var json = await data.Content.ReadAsStringAsync();
-            var apiData = JsonSerializer.Deserialize<ApiData>(json);
+            var apiData = JsonSerializer.Deserialize<ApiData>(json)
+                ?? throw new InvalidOperationException("Could not deserialize response from NitroType.");
 
-            _cache = apiData;
-            _lastQueriedAt = DateTime.UtcNow;
+            apiData.Timestamp = DateTime.UtcNow;
+            _data.Add(apiData);
         }
 
-        if (DateTime.UtcNow - _previousDataQueriedAt > TimeSpan.FromDays(1))
-        {
-            _previousData = _cache;
-            _previousDataQueriedAt = DateTime.UtcNow;
-        }
+        var previous = _data.Where(x => x.Timestamp < DateTime.UtcNow - _deltaInterval).FirstOrDefault();
+        if (previous == null) previous = _data.First();
+        var current = _data.Last();
 
-        /*foreach (var catInfo in _cache.results.season)
+        var result = _data.Last().results.season.Select(cat =>
         {
-            var previous = _previousData.results.season.FirstOrDefault(x => x.username == catInfo.username);
-            if (previous != null)
+            var previousCat = previous.results.season.FirstOrDefault(x => x.username == cat.username);
+            if (previous == null)
+                return cat with { CurrentDelta = new() };
+
+            return cat with
             {
-                catInfo.AccuracyImprovement = catInfo.Accuracy - previous.Accuracy;
-                catInfo.AverageSpeedImprovement = catInfo.AverageSpeed - previous.AverageSpeed;
-            }
-        }*/
+                CurrentDelta = new Delta
+                {
+                    Accuracy = cat.Accuracy - previousCat.Accuracy,
+                    Speed = cat.AverageSpeed - previousCat.AverageSpeed,
+                    CharactersTyped = cat.typed - previousCat.typed,
+                    Races = cat.played - previousCat.played
+                }
+            };
+        }).ToList();
 
-        return _cache.results.season;
+        if (result.Any(x => x.CurrentDelta.CharactersTyped < 0))
+        {
+            Console.WriteLine("Erasing season, it has ended.");
+            _data = [];
+            return await GetCatsInfoAsync();
+        }
+
+        return result;
     }
 }
 
-public sealed record ApiData(ApiResults results);
+public sealed record ApiData(ApiResults results)
+{
+    public DateTime Timestamp { get; set; }
+}
 
 public sealed record ApiResults(CatInfo[] season);
 
@@ -54,6 +88,8 @@ public sealed record CatInfo(string username, string displayName, long typed, lo
     public decimal AverageSpeed => (60m / 5) * typed / secs;
     public decimal AccuracyImprovement { get; set; } = 0m;
     public decimal AverageSpeedImprovement { get; set; } = 0m;
+
+    public Delta CurrentDelta { get; set; }
 
     public string TimeSpent
     {
@@ -74,32 +110,41 @@ public sealed record CatInfo(string username, string displayName, long typed, lo
         }
     }
 
-    public string AccuracyImprovementString
-    {
-        get
-        {
-            if (AccuracyImprovement > 0) return $"\u25b2 {AccuracyImprovement.ToString("#.#####")}";
-            if (AccuracyImprovement < 0) return $"\u25bc {(-AccuracyImprovement).ToString("#.#####")}";
-
-            return string.Empty;
-        }
-    }
-
-    public string AverageSpeedImprovementString
-    {
-        get
-        {
-            if (AverageSpeedImprovement > 0) return $"\u25b2 {AverageSpeedImprovement.ToString("#.#####")}";
-            if (AverageSpeedImprovement < 0) return $"\u25bc {(-AverageSpeedImprovement).ToString("#.#####")}";
-
-            return string.Empty;
-        }
-    }
-
     public string GetDeltaClass(decimal value)
     {
         if (value > 0) return "good";
         if (value < 0) return "bad";
+        return string.Empty;
+    }
+
+
+}
+
+public sealed record Delta
+{
+    public decimal Accuracy { get; set; }
+    public decimal Speed { get; set; }
+    public long CharactersTyped { get; set; }
+    public long Races { get; set; }
+
+    public string AccuracyString => ValueToString(Accuracy);
+    public string SpeedString => ValueToString(Speed);
+    public string CharactersTypedString => LongToString(CharactersTyped);
+    public string RacesString => LongToString(Races);
+
+    private string LongToString(long value)
+    {
+        if (value > 0) return $"\u25b2 {value.ToString()}";
+        if (value < 0) return $"\u25bc {(-value).ToString()}";
+
+        return string.Empty;
+    }
+
+    private string ValueToString(decimal value)
+    {
+        if (value > 0) return $"\u25b2 {value.ToString("#.#####")}";
+        if (value < 0) return $"\u25bc {(-value).ToString("#.#####")}";
+
         return string.Empty;
     }
 }
