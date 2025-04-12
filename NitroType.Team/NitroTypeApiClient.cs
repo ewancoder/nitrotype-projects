eb12a0a5d1ca08fba75b10db268516bc5608a096
Ivan Zyranau <ewancoder@gmail.com>
2025-04-12 19:55:33 +0400

    Change titles to be more attainable over the course of the league

--- NitroType.Team/NitroTypeApiClient.cs
+++ NitroType.Team/NitroTypeApiClient.cs
@@ -123,12 +123,12 @@ public sealed record CatInfo(string username, string displayName, long typed, lo
 
     public string GetRacesClass()
     {
-        if (played >= 2000) return "thebest";
-        if (played >= 1000) return "awesome";
-        if (played >= 500) return "epic";
-        if (played >= 250) return "unique";
-        if (played >= 100) return "rare";
-        if (played >= 30) return "magic";
+        if (played >= 1000) return "thebest";
+        if (played >= 500) return "awesome";
+        if (played >= 250) return "epic";
+        if (played >= 100) return "unique";
+        if (played >= 50) return "rare";
+        if (played >= 20) return "magic";
 
         return "getting there";
     }
